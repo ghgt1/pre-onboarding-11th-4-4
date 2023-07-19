da632f45f7a3e20dd23106958137b9d7533c542c
JYROH <ghgt1@naver.com>
2023-07-19 14:07:52 +0900

    feat: api로직에 캐쉬 검증 로직 추가

--- src/api/search.ts
+++ src/api/search.ts
@@ -1,9 +1,14 @@
 import instance from './axios';
 import { Sick } from '../types/SickType';
+import { getCache, setCache } from '../utils/cacheStorage';
 
 export async function getSickList(search: string): Promise<Sick[]> {
   try {
+    const cachedResponse = await getCache(search);
+    if (cachedResponse) return cachedResponse.json();
+    console.info('calling api');
     const response = await instance.get(`sick?q=${search}`);
+    await setCache(search, response.data);
     return response.data;
   } catch (error) {
     console.log(error);
