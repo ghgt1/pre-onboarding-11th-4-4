12fe80171bbf325a17f250e38fc93cd673447657
JYROH <ghgt1@naver.com>
2023-07-19 14:07:35 +0900

    feat: 상수에 캐쉬 만료시간 설정

--- src/constants/constant.ts
+++ src/constants/constant.ts
@@ -1,3 +1,6 @@
-export const recommendArray = Object.freeze(['B형간염', '비만', '관절염', '우울증', '식도염']);
+export const RECOMMEND_ARRAY = Object.freeze(['B형간염', '비만', '관절염', '우울증', '식도염']);
 
-export const maxShowNum = 7;
+export const MAX_SHOW_NUM = 7;
+
+// 24시간
+export const EXPIRE_TIME = 24 * 60 * 60 * 1000;
